a34cde100509856be415799267436f2336620257
Julian Waller <git@julusian.co.uk>
2022-11-25 21:52:08 +0000

    chore

--- src/util.ts
+++ src/util.ts
@@ -6,10 +6,6 @@ export function assertUnreachable(_never: never): void {
 	// throw new Error('Unreachable')
 }
 
-export function literal<T>(val: T): T {
-	return val
-}
-
 export function padNumber(i: number, len = 2): string {
 	return ('000000' + i).substr(-len)
 }
